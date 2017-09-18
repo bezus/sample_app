7bed98ceb93608536d1c3d88305418227dd877b3
bezus <chahinezaraibia@gmail.com>
2017-09-18 15:41:19 +0100

    add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,36 +5,36 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
- def setup 
-   @user =User.new(name:"Example User", email: "user@exemple.com", 
+ def setup
+   @user =User.new(name:"Example User", email: "user@exemple.com",
                                      password:"foobar",password_confirmation:"foobar")
- end 
- test "should be valid" do 
+ end
+ test "should be valid" do
   assert @user.valid?
- end 
+ end
  test "name should be present " do
    @user.name="  "
    assert_not @user.valid?
- end 
+ end
  test "email should be present " do
    @user.email="  "
    assert_not @user.valid?
- end 
- test "name shouldn't be too long " do 
+ end
+ test "name shouldn't be too long " do
    @user.name="a"*51
    assert_not @user.valid?
- end 
- test "email shouldn't be too long " do 
+ end
+ test "email shouldn't be too long " do
    @user.email = "a"*244 + "example.com"
    assert_not @user.valid?
  end
  test "email validation sould accept valid addresses" do
-   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
+   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                         first.last@foo.jp alice+bob@baz.cn]
    valid_addresses.each do |valid_address|
     assert @user.valid?, "#{valid_address.inspect} should be valid "
-   end 
- end 
+   end
+ end
  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
@@ -43,13 +43,13 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
- test "email addresses should be unique " do 
-  duplicate_user = @user.dup 
-  duplicate_user.email= @user.email.upcase    
-  @user.save 
+ test "email addresses should be unique " do
+  duplicate_user = @user.dup
+  duplicate_user.email= @user.email.upcase
+  @user.save
    assert_not duplicate_user.valid?
- end 
- test "email adresses should be saved as lower-case " do 
+ end
+ test "email adresses should be saved as lower-case " do
    mixed_case_email = "Foo@ExAPLE.cOM"
    @user.email = mixed_case_email
    @user.save
@@ -59,8 +59,11 @@ class UserTest < ActiveSupport::TestCase
   @user.password = @user.password_confirmation=" "*6
   assert_not @user.valid?
  end
-  test "password should have minimum length "do 
+  test "password should have minimum length "do
     @user.password=@user.password_confirmation = "a"*5
-   assert_not @user.valid? 
- end 
-end 
+   assert_not @user.valid?
+ end
+ test "authenticated? should return false for a user with nil digest" do
+   assert_not @user.authenticated?(:remember, '')
+ end
+end
